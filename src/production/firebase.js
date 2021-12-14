48a2d54efa9e94ac209af4ed38e47830be22a9e3
mushahid <mushahidmehdi14@gmail.com>
2021-12-14 15:26:05 +0300

    add all

---
+++ src/production/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCZQN8t_egAz3znz8gLZkcXRUGSk-JbP8g",
+	authDomain: "weather-5bf43.firebaseapp.com",
+	projectId: "weather-5bf43",
+	storageBucket: "weather-5bf43.appspot.com",
+	messagingSenderId: "215409634183",
+	appId: "1:215409634183:web:d50c4cc3f3d11676bf0122"
+  };
